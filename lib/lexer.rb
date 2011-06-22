16203dedc67ea5872a8b227c3982590b861c04ba
asterite <ary@esperanto.org.ar>
2011-06-22 13:18:44 +0700

    Lex numbers

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -3,5 +3,6 @@ require 'rltk/lexer'
 class Lexer < RLTK::Lexer
   rule(/\s/)
   rule(/def/) { [:IDENT, :def] }
-  rule(/\w+/) { |id| [:IDENT, id] }
+  rule(/\d+/) { |num| [:INT, num.to_i] }
+  rule(/[a-zA-Z_][a-zA-Z_0-9]*/) { |id| [:IDENT, id] }
 end
--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -10,5 +10,19 @@ describe Lexer do
     end
   end
 
+  def self.it_lexes_idents(*args)
+    args.each do |arg|
+      it_lexes arg, :IDENT, arg
+    end
+  end
+
+  def self.it_lexes_ints(*args)
+    args.each do |arg|
+      it_lexes arg, :INT, arg.to_i
+    end
+  end
+
   it_lexes "def", :IDENT, :def
+  it_lexes_idents "ident", "something", "with_underscores", "with_1"
+  it_lexes_ints "1", "1hello"
 end
