5734fde9830ea39727ff5aaa3e46993ab6e67baa
asterite <ary@esperanto.org.ar>
2011-06-25 16:04:58 +0700

    Better CLI

--- lib/crystal/cli.rb
+++ lib/crystal/cli.rb
@@ -3,16 +3,44 @@ require(File.expand_path("../../../lib/crystal",  __FILE__))
 mod = Crystal::Module.new
 result = nil
 
+def count_openings(string)
+  openings = 0
+
+  lexer = Crystal::Lexer.new string
+  while (token = lexer.next_token).type != :EOF
+    if token.type == :IDENT
+      if token.value == :def
+        openings += 1
+      elsif token.value == :end
+        openings -= 1
+      end
+    end
+  end
+  openings
+end
+
+buffer = ""
+openings = 0
+
 loop do
   print "crystal > "
-  line = gets.strip
-  next if line.empty?
-  break if line == "exit" || line == "quit"
+  print ("  " * openings)
+  line = gets
+  next if line.strip.empty?
 
-  begin
-    result = mod.eval line
-    puts " => #{result ? result : 'nil'}"
-  rescue => ex
-    puts ex
+  buffer << line
+  openings = count_openings buffer
+
+  if openings == 0
+    break if line.strip == "exit" || line.strip == "quit"
+
+    begin
+      result = mod.eval buffer
+      puts " => #{result ? result : 'nil'}"
+    rescue => ex
+      puts ex
+    end
+
+    buffer = ""
   end
 end
