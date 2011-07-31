d0e049474b60de6ab36a145e9729a997f09b7c9a
asterite <ary@esperanto.org.ar>
2011-07-31 00:55:41 -0300

    Renamed instantiated methods

--- lib/crystal/instance.rb
+++ lib/crystal/instance.rb
@@ -4,21 +4,21 @@ module Crystal
       return self if resolved_type
 
       args_types = node.args.map &:resolved_type
-      args_types_signature = args_types.join '$'
+      args_types_signature = args_types.join ', '
       args_values = []
       args_values_signature = ""
       args.each_with_index do |arg, i|
         if arg.constant?
           raise "Argument #{arg} must be known at compile time" unless node.args[i].can_be_evaluated_at_compile_time?
           arg_value = scope.eval_anon node.args[i]
-          args_values_signature << "$" unless args_values_signature.empty?
+          args_values_signature << ", " unless args_values_signature.empty?
           args_values_signature << arg_value.to_s
           args_values << arg_value
         else
           args_values << nil
         end
       end
-      instance_name = "#{name}$#{args_types_signature}$#{args_values_signature}$"
+      instance_name = instance_name name, args_types_signature, args_values_signature
 
       instance = scope.find_expression instance_name
       if !instance || node.block
@@ -26,7 +26,6 @@ module Crystal
         instance_args.each_with_index { |arg, index| arg.compile_time_value = args_values[index] }
         args_types.each_with_index { |arg_type, i| instance_args[i].resolved_type = arg_type }
         instance = Def.new instance_name, instance_args, body.clone
-        scope.add_expression instance
       end
 
       if node.block
@@ -41,12 +40,20 @@ module Crystal
 
         scope.remove_expression instance
 
-        instance.name = "#{name}$#{args_types_signature}$#{block.resolved_type}$#{args_values_signature}$"
+        instance.name = instance_name name, args_types_signature, args_values_signature, block.resolved_type
         instance.block = block
       else
         instance.accept resolver
       end
       instance
+    end
+
+    def instance_name(name, args_types, args_values, block_type = nil)
+      i = "#{name}"
+      i << "<#{args_types}>" if args_types.length > 0
+      i << "(#{args_values})" if args_values.length > 0
+      i << "&#{block_type}" if block_type
+      i
     end
   end
 end
